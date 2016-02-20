e5a80737b42dd8f2a1f8ca5333799541dcfad04a
Bruce Chen <bruce@beansmile.com>
2016-02-20 18:41:56 +0800

    Test react native camera commpont, and replace default app icon

--- index.ios.js
+++ index.ios.js
@@ -6,47 +6,57 @@
 import React, {
   AppRegistry,
   Component,
+  Dimensions,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
+
+import Camera from 'react-native-camera';
 
 class Ledoutong extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.stretch}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
       </View>
     );
+  }
+
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  preview: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
 });
 
 AppRegistry.registerComponent('Ledoutong', () => Ledoutong);
