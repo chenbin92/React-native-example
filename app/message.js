5b8873427ade639ae7f6f827b2ccf05173b7763e
Bruce Chen <bruce@beansmile.com>
2016-03-04 21:58:12 +0800

    Listview component example1

--- app/message.js
+++ app/message.js
@@ -15,111 +15,36 @@ var {
 } = React;
 
 var MessageDetail = require('./messageDetail');
-var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
-var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
-var PAGE_SIZE = 25;
-var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
-var REQUEST_URL = API_URL + PARAMS;
 
-var Message = React.createClass({
+var ListViewExample = React.createClass({
   // 这里返回一个对象，设置组件的初始化状态，
   // 后面就可以通过 this.state 来获得这个对象
   getInitialState: function() {
+    var ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2  // 用于判断当前数据变化的标准
+    });
     return {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
+      dataSource: ds.cloneWithRows([ // cloneWithRows方法用于复制克隆，相当于拷贝一份当前数据
+        'row 1', 'row 2','row 3','row 4','row 5','row 6','row 7','row 8','row 9','row 10','row 11', 'row 12'
+      ]),
     };
   },
-
-  // 这里一般做加载数据的操作
-  componentDidMount: function() {
-    this._fetchData();
-  },
-
-  // 使用 fetch api 获取数据
-  // 通过 this.setState 方法来修改组件的状态值
-  _fetchData: function() {
-    fetch(REQUEST_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-          loaded: true,
-        });
-      })
-      .done();
-  },
-
-  _renderLoadingView: function() {
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading movies...
-        </Text>
-      </View>
-    );
-  },
-
-  _renderMovie: function(movie) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail} />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
-      </View>
-    );
-  },
-
   render: function() {
-    console.log(this.state)
-    console.log("REQUEST_URL: " + REQUEST_URL)
-    if (!this.state.loaded) {
-      return this._renderLoadingView();
-    }
-
     return (
       <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this._renderMovie}
-        style={styles.listView} />
+        dataSource={this.state.dataSource} // 数据源
+        renderRow={(rowData) => <Text style={styles.rowText}>{rowData}</Text>} /> // 接受数组中的每个数据作为参数, 作为listview的每一行（for循环）
     );
-  },
+  }
 });
 
 var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  rightContainer: {
-    flex: 1,
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center',
-  },
-  year: {
-    textAlign: 'center',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF',
+  rowText: {
+    padding: 18,
+    borderWidth: 1,
+    borderColor: '#eee'
   },
 });
 
 
-module.exports = Message;
+module.exports = ListViewExample;
